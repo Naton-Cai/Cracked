d9004df04bf990fca106530c855030497ab577cd
Naton-Cai <natoncai@gmail.com>
2023-08-01 13:01:18 -0700

    More Changes

---
+++ Player1.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+@export var move_speed : float = 600.0
+@export var jump_force : float = 800.0
+@export var gravity : float = 800.0
+@export var fall_force : float = 800.0
+@export var fast_fall : bool = false
+@export var direction = 1
+@export var size = 0.2
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta 
+	
+	if is_on_floor():
+		fast_fall = false
+		
+	velocity.x = 0
+	
+	if Input.is_action_pressed("P1L"):
+		velocity.x -= move_speed
+		direction = -1
+		get_node( "Sprite2D").scale.x = size  * direction
+		#get_node("Hitbox").scale.x = direction
+		
+	if Input.is_action_pressed("P1R"):
+		velocity.x += move_speed
+		direction = 1
+		get_node("Sprite2D").scale.x = size  * direction
+		#get_node("Hitbox").scale.x = direction
+	
+	if Input.is_action_just_pressed("P1U") and is_on_floor() == true:
+			velocity.y = -jump_force
+	
+	if Input.is_action_just_pressed("P1D") and is_on_floor() == false and fast_fall == false:
+		fast_fall = true
+		velocity.y = fall_force
+
+	move_and_slide()
